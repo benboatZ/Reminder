3086f10ccaed32dd1e92fec7432900678d078272
benboat <benboat@163.com>
2017-03-07 18:16:04 +0800

    Initial commit using new project wizard

---
+++ app/src/main/java/com/sy/benboat/reminders/RemindersActivity.java
@@ -0,0 +1,13 @@
+package com.sy.benboat.reminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+    }
+}
