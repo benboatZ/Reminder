d4f3dff42c65c46d198205cb48f9d7c202681202
benboat <benboat@163.com>
2017-03-08 14:49:05 +0800

    Add DATABASE Persistent

--- app/src/main/java/com/sy/benboat/reminders/RemindersActivity.java
+++ app/src/main/java/com/sy/benboat/reminders/RemindersActivity.java
@@ -1,14 +1,16 @@
 package com.sy.benboat.reminders;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class RemindersActivity extends AppCompatActivity {
     private ListView mListView;
+    private RemindersDbAdapter mDbAdapter;
+    private ReminderSimpleCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,36 @@ public class RemindersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reminders);
 
         mListView = (ListView) findViewById(R.id.Reminders_list_view);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+       /* ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.reminders_row,
                 R.id.row_text,
                 new String[]{"first record", "second record", "third record"});
-        mListView.setAdapter(arrayAdapter);
+        mListView.setAdapter(arrayAdapter);*/
+        mListView.setDivider(null);
+        mDbAdapter=new RemindersDbAdapter(this);
+        mDbAdapter.open();
+
+        Cursor cursor=mDbAdapter.fetchAllReminders();
+
+        String[] from=new String[]{
+                RemindersDbAdapter.COL_CONTENT
+        };
+
+        int[] to=new int[]{
+                R.id.row_text
+        };
+
+        mCursorAdapter=new ReminderSimpleCursorAdapter(
+                RemindersActivity.this,
+                R.layout.reminders_row,
+                cursor,
+                from,
+                to,
+                0
+        );
+
+        mListView.setAdapter(mCursorAdapter);
     }
 
     @Override
